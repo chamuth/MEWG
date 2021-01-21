0eb996121a08a142b8029aeb760feb59078dab0d
Chamuth Chamandana <c.chamandana@gmail.com>
2021-01-21 19:02:24 +0530

    Next round view

---
+++ Assets/Scripts/UI/ShineAnimator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI.Extensions;
+
+public class ShineAnimator : MonoBehaviour
+{
+    ShineEffector shiner;
+
+    private void Start()
+    {
+        shiner = GetComponent<ShineEffector>();
+        shiner.YOffset = -1;
+    }
+
+    private float tempTimeCounter = 2f;
+
+    private void Update()
+    {
+        if (shiner.YOffset < 1)
+        {
+            shiner.YOffset += Time.deltaTime * 3f;
+        }
+        else
+        {
+            if (tempTimeCounter > 0)
+            {
+                tempTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                tempTimeCounter = 2f;
+                shiner.YOffset = -1;
+            }
+        }
+    }
+}
