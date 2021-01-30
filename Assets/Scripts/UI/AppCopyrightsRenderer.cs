bb9105e01815a4e0d66a4b985d5cea5ba42c9e14
Chamuth Chamandana <c.chamandana@gmail.com>
2021-01-30 23:44:09 +0530

    Version and copyrights message renderers

---
+++ Assets/Scripts/UI/AppCopyrightsRenderer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppCopyrightsRenderer : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<TMPro.TextMeshProUGUI>().text = string.Format("© {0} Ninponix Games", DateTime.Now.Year);
+    }
+}
---
+++ Assets/Scripts/UI/AppVersionRenderer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppVersionRenderer : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<TMPro.TextMeshProUGUI>().text = "v" + Application.version;
+    }
+}
