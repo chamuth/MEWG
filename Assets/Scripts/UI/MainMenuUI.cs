4756c02a5ba2ab695b5466c4fcfdd703fe08327d
Chamuth Chamandana <c.chamandana@gmail.com>
2021-02-12 14:54:29 +0530

    Randomized the free hint ad

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -63,7 +63,7 @@ public class MainMenuUI : MonoBehaviour
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
 
         // Awarded the second time player launched main menu after logging in 
-        if (auth.CurrentUser != null)
+        if (auth.CurrentUser != null && UnityEngine.Random.Range(0,10f) > 7f)
             RewardFreeHints();
     }   
 
